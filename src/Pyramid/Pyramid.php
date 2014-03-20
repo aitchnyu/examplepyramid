07ccc93add77fe897f73f043cdc0bea919e1ba27
Jesvin Jose <jjvattamattom@suyati.com>
2014-03-20 10:27:38 +0530

    pyramids, yay

---
+++ src/Pyramid/Pyramid.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Pyramid;
+
+class Pyramid
+{
+    public function generatePyramid($steps){
+        $buffer = "";
+        for($i = 0; $i < $steps; $i++){
+            for($j = $steps - $i; $j > 0; $j--){
+                $buffer = $buffer." ";
+            }
+            for( $j = 0; $j < 2 * $i + 1; $j++){
+                $buffer = $buffer."*";
+            }
+            $buffer = $buffer."\n";
+        }
+        return $buffer;
+    }
+}
+
+?>
